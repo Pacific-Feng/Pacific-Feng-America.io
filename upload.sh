81e60a9d874460a35959c2928641732e237c5f79
謝旻峰 <tony32217@gmail.com>
2022-02-10 15:40:07 +0800

    Edited at 2022/02/025

--- upload.sh
+++ upload.sh
@@ -1,12 +1,3 @@
 #!bin/bash
-comment_name="Edited at 2022/02/025"
-cd /d/Github/Pacific-Feng.io/
-git add .
-git commit -m "$comment_name"
-git push
-git pull
-cd /d/Github/Pacific-Feng-America.io/
-git add .
-git commit -m "$comment_name"
-git push
-git pull
+cd /d/Github/Upload/
+sh upload.sh
